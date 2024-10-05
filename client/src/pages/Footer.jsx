519487d7da5a24ce5b0c248ee7d45d33335c32ad
Miroslav Ivanov <ivanov.miroslav2023@gmail.com>
2024-10-05 20:56:59 +0100

    Add initials

--- client/src/pages/Footer.jsx
+++ client/src/pages/Footer.jsx
@@ -1,7 +1,12 @@
 export default function Footer() {
     return (
-        <div className="h-[3rem] flex justify-center items-center bg-[#1f2937]">
-            <p className="font-semibold text-center text-white">Copyright © 2024 BRS Industry </p>
+        <div className="h-[3rem] flex justify-evenly items-center bg-[#1f2937]">
+            <p className="font-semibold text-center text-white">
+                Copyright © 2024 BRS Industry{" "}
+            </p>
+            <p className="font-semibold text-center text-white">
+                Created by Miroslav{" "}
+            </p>
         </div>
     );
 }
