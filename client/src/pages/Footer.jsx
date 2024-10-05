3ce6aec5f65b63bb95e7eff10c906d3ef9ba6bf2
Miroslav Ivanov <ivanov.miroslav2023@gmail.com>
2024-10-05 21:36:44 +0100

    Fix text size

--- client/src/pages/Footer.jsx
+++ client/src/pages/Footer.jsx
@@ -1,10 +1,10 @@
 export default function Footer() {
     return (
         <div className="h-[3rem] flex justify-evenly items-center bg-[#1f2937]">
-            <p className="font-semibold text-center text-white">
+            <p className="font-semibold text-center text-white text-[0.7rem] md:text-base">
                 Copyright © 2024 BRS Industry{" "}
             </p>
-            <p className="font-semibold text-center text-white">
+            <p className="font-semibold text-center text-white text-[0.7rem] md:text-base">
                 Created by Miroslav{" "}
             </p>
         </div>
